85e2c1d637e2d0ced5b0caf3ac3c62ce1d61bd19
Ken Franken <kfranken@titantv.com>
2011-04-06 11:05:34 -0500

    First pass at Fibonacci sequence. Only works for small integers, gets really slow around i=40.

--- TestApplication/Foo.cs
+++ TestApplication/Foo.cs
@@ -21,6 +21,14 @@ namespace TestApplication
             copy = null;
         }
 
+        public static int Fib(int n)
+        {
+            if (n < 2)
+                return 1;
+            else
+                return Fib(n - 1) + Fib(n - 2);
+        }
+
     }
 
 }
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -9,6 +9,8 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 100; i++)
+                Console.WriteLine("{0}\t{1}", i, Foo.Fib(i));
         }
     }
 }
