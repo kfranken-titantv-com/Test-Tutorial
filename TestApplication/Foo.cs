4ff474c52cb14f67b2c41ed4c358a2f0be28a548
Ken Franken <kfranken@titantv.com>
2011-04-06 11:10:25 -0500

    Fix bug in Fibonacci sequence for i = 0.

--- TestApplication/Foo.cs
+++ TestApplication/Foo.cs
@@ -23,7 +23,9 @@ namespace TestApplication
 
         public static int Fib(int n)
         {
-            if (n < 2)
+            if (n == 0)
+                return 0;
+            else if (n == 1)
                 return 1;
             else
                 return Fib(n - 1) + Fib(n - 2);
